6ce5b603fbe9272a97a833656a6cd9b45863f6b7
sabirmostofa <sabirmostofa@gmail.com>
2021-08-01 23:28:15 +0200

    slack check

--- src/main/java/com/tools/blog/controllers/Post.java
+++ src/main/java/com/tools/blog/controllers/Post.java
@@ -1,5 +1,22 @@
 package com.tools.blog.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class Post {
+
+
+    /**
+     * Get all posts
+     * @return
+     */
+    @GetMapping("/posts")
+    public Map<Integer, String> getAllPosts(){
+
+        HashMap<Integer, String> map = new HashMap<>();
+        return map;
+    }
     
 }
